651dc732c679cdcda58be0dbe4f1bb8c9353124c
antonionsx1 <antonionsx@gmail.com>
2021-06-30 09:38:17 -0500

    mejora visual

--- index.php
+++ index.php
@@ -1,30 +1,32 @@
 <!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" crossorigin="anonymous">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link rel="stylesheet" href="style.css">
         <title>PHP para principiantes</title>
     </head>
     <body>
         <div class="container">
             <div class="row">
-                <div class="col-xl-2 bd-sidebar border"><?php include("./menu.php") ?></div>
+                <div class="col-xl-2 bd-sidebar border-end"><?php include("./menu.php") ?></div>
                 <?php
-                $url = "";
                 if (isset($_GET["action"])) {
                     include $_GET["action"];
                 } else {
                     ?>
-                    <div class="col-xl-10 index"></div>
+                    <div class="col-xl-10">
+                        <h1 class="text-center p-3">Bienvendios a Php para principiantes</h1>
+                    </div>
                     <?php
                 }
                 ?>
             </div>
         </div>
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
 </html>
--- language/control-structures/inclusion_ficheros.php
+++ language/control-structures/inclusion_ficheros.php
@@ -1,40 +1,15 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- Bootstrap CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-        <link rel="stylesheet" href="style.css">
-        <title>Prueba de PHP</title>
-    </head>
-    <body>
-        <div class="container">
-            <div class="row">
-                <div class="col-xl-2 bd-sidebar border">
-                    <?php include($_SERVER['DOCUMENT_ROOT'] . '/php-principiantes/menu.php'); ?>
-                </div>
-                <div class="col-xl-10">
-                    <h1>Inclusión de ficheros PHP</h1>
-                    <code>
-                        <pre>
-                            require('/directorio/fichero');
-                            include('/directorio/fichero');
-                            require_once('/directorio/fichero');
-                            include_once('/directorio/fichero');
-                        </pre>
-                    </code>
-                    <code>require</code> produce un error fatal y la terminación del script si falla.<br>
-                    <code>include</code> solo produce un warning.<br>
-                    <code>include_once</code> y <code>require_once</code> para incluir definiciones estáticas.
-                    <?= $menu_nombre ?>
-                </div>
-            </div>
-        </div>
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
-        <!-- Option 1: Bootstrap Bundle with Popper -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-    </body>
-</html>
-
+<div class="col-xl-10">
+    <h1>Inclusión de ficheros PHP</h1><br>
+    <code>
+        <pre>
+        require('/directorio/fichero');
+        include('/directorio/fichero');
+        require_once('/directorio/fichero');
+        include_once('/directorio/fichero');
+        </pre>
+    </code>
+    <code>require</code> produce un error fatal y la terminación del script si falla.<br>
+    <code>include</code> solo produce un warning.<br>
+    <code>include_once</code> y <code>require_once</code> para incluir definiciones estáticas.
+    <?= $menu_nombre ?>
+</div>
