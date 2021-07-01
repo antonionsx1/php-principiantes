b164c18bf8d0f98ac901f414ea3c61151d6b5636
antonionsx1 <antonionsx@gmail.com>
2021-07-01 12:32:36 -0500

    add index2

---
+++ index_1.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+        <title>Php para principiantes!</title>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xl-3 border-end"><?php include './menu.php'; ?></div>
+                <div class="col-xl-9">
+                    <h1 class="text-center p-3">Bienvenidos a Php para principiantes</h1>
+                </div>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
